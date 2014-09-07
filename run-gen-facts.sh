6e98c051b42930174e3402466c6a666d63b0607e
Sohan Jain <sjain@localytics.com>
2014-09-07 13:06:10 -0400

    fix ssh in run-gen-facts

--- run-gen-facts.sh
+++ run-gen-facts.sh
@@ -6,7 +6,7 @@ source tpcds-env.sh
 
 cat dn.txt | while read h
 do 
-  ssh $h "cd $CLUSTER_HOMEDIR/$MPP_TPCDS_DIR; ./gen-facts.sh" < /dev/null &
+  ssh $CLUSTER_USER:$h "cd $CLUSTER_HOMEDIR/$MPP_TPCDS_DIR; ./gen-facts.sh" < /dev/null &
 done
 wait
 
