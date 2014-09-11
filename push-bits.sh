957f91cb43cdac4618f02be5e01e5f8d4035f72f
Sohan Jain <sjain@localytics.com>
2014-09-11 12:31:07 -0400

    push bits to correct dir

--- push-bits.sh
+++ push-bits.sh
@@ -8,7 +8,7 @@ source tpcds-env.sh
 
 cat dn.txt | while read h
 do
-  scp $SSH_OPTS -rp "$HOMEDIR/$MPP_TPCDS_DIR" $CLUSTER_USER@$h:$CLUSTER_HOMEDIR/$MPP_TPCDS_DIR
+  scp $SSH_OPTS -rp "$HOMEDIR/$MPP_TPCDS_DIR" $CLUSTER_USER@$h:$CLUSTER_HOMEDIR
   ssh $SSH_OPTS $CLUSTER_USER@$h "cd $CLUSTER_HOMEDIR/$MPP_TPCDS_DIR; ./init-tpcds.sh" &
 done
 wait
